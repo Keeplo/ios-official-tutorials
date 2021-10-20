aa12b21dbf43439097ad995e7ee2578de2706c3b
YongwooKim <keepingitflow@gmail.com>
2021-10-20 17:35:19 +0900

    Chapter 1 - Building Lists and Navigation - Section 1 Chapter 1: Essentials Building Lists and Navigation Section 1 : Create a Landmark Model

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  Landmarks
+//
+//  Created by Yongwoo Marco on 2021/10/20.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
