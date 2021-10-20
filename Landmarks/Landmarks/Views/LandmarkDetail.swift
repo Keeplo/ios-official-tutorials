e9b608621373446333b10f57fc03c8a28f6b3c1c
YongwooKim <keepingitflow@gmail.com>
2021-10-20 18:23:22 +0900

    Chapter 1 - Building Lists and Navigation - Section 6 Chapter 1: Essentials Building Lists and Navigation Section 6 : Set Up Navigation Between List and Detail

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Yongwoo Marco on 2021/10/20.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -120)
+            
+            VStack(alignment: .leading) {
+                Text("Turtie Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
